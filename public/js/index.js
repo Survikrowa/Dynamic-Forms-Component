dd04c2086bcf16636d045265e184d78dfb69ada5
Szymon Hrabia <survikrowa@o2.pl>
2019-12-27 13:11:48 +0100

    Commit do PR: Nie działa usuwanie ani walidacja wszystkich inputów

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,14 @@
 const addButton = document.getElementById("js-button-add");
-const mainForm = document.getElementById("js-form");
 const inputInForm = document.getElementById("js-input");
 const deleteButton = document.getElementById("js-delete-button");
 const fieldsWrapper = document.getElementById("js-fields-wrapper");
+let inputArray = document.querySelectorAll("#js-input");
 
 let indexPlus = 1;
+
+const getMainFormFromDom = () => {
+  return (mainForm = document.getElementById("js-form"));
+};
 
 const increaseIndex = () => {
   indexPlus++;
@@ -13,59 +17,77 @@ const increaseIndex = () => {
 };
 const renderInput = () => {
   return (innerHTML = `<div class="form__wrapper" id="js-form-input">
-  <div class="form__field">
+  <div class="form__field" id="js-field">
     <label class="form__label">Haslo ${indexPlus}</label>
     <input type="text" class="form__input" id="js-input" data-index=${indexPlus} />
   </div>
   <button type="button" class="btn__field" id="js-delete-button">
-    <img src="../css/icon/close.svg" class="img--size" />
+    <img src="/close.a5d26944.svg"" class="img--size" />
   </button>
   </div>`);
 };
 
 const renderValidationError = () => {
-  return (innerHTML = `<span style="color: red;">Input jest pusty! Uzupełnij go prosze</span>`);
+  return (innerHTML = `<span style="color: red;" id="js-error-span">Input jest pusty! Uzupełnij go prosze</span>`);
+};
+
+const getInputErrorElement = () => {
+  return (inputErrorSpan = document.getElementById("js-error-span"));
+};
+
+const clearValidationError = () => {
+  return (inputErrorSpan.innerHTML = "");
 };
 
 const getQuantityOfInputs = () => {
+  getMainFormFromDom();
   return (inputsNumber = mainForm.querySelectorAll("input").length);
 };
 
 const getValueOfInputs = () => {
-  return (inputsValue = mainForm.querySelectorAll("input").value);
+  getMainFormFromDom();
+  return (inputsValue = mainForm.querySelector("input").value);
 };
 
-const inputValidation = () => {
-  getValueOfInputs();
-  console.log(inputsValue);
-  if (inputsValue == undefined) {
-    fieldsWrapper.insertAdjacentHTML("beforeend", renderValidationError());
-    addButton.disabled = true;
-    return (validate = false);
-  } else {
-    return (validate = true);
-    fieldsWrapper.insertAdjacentHTML("beforeend", "<span></span>");
-  }
+const inputsValidation = () => {
+  inputArray.forEach(() => {
+    getValueOfInputs();
+    console.log(inputsValue);
+    if (inputsValue === "") {
+      fieldsWrapper.insertAdjacentHTML("beforeend", renderValidationError());
+      addButton.disabled = true;
+      return (isValid = false);
+    } else {
+      getInputErrorElement();
+      if (inputErrorSpan) {
+        fieldsWrapper.insertAdjacentHTML("beforeend", clearValidationError());
+      }
+      return (isValid = true);
+    }
+  });
 };
 
-// const cursorMove = () => {
-//   inputInForm.focus();
-//   inputInForm.setSelectionRange(input, 1);
-// };
+const cursorMove = () => {
+  inputInForm.focus();
+};
 
 const addBtnHandler = () => {
-  inputValidation();
+  inputsValidation();
   getQuantityOfInputs();
   if (inputsNumber >= 5) {
     addButton.disabled = true;
   }
   console.log(indexPlus);
-  if (validate == true) {
+  if (isValid == true) {
     increaseIndex();
     fieldsWrapper.insertAdjacentHTML("beforeend", renderInput(indexPlus));
+    cursorMove();
   }
 };
-inputInForm.addEventListener("change", () => {
-  addButton.disabled = false;
+inputArray.forEach(() => {
+  inputInForm.addEventListener("change", () => {
+    addButton.disabled = false;
+  });
 });
+
 addButton.addEventListener("click", addBtnHandler);
