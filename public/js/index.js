ee2f975b3504eeaa557e40f4ff6471161ba3b38c
Survikrowa <survikrowa@o2.pl>
2019-12-27 11:11:46 +0100

    Another files move

--- public/js/index.js
+++ public/js/index.js
@@ -1,33 +1,71 @@
 const addButton = document.getElementById("js-button-add");
 const mainForm = document.getElementById("js-form");
-const inputInForm = document.getElementById("js-form-input");
+const inputInForm = document.getElementById("js-input");
 const deleteButton = document.getElementById("js-delete-button");
 const fieldsWrapper = document.getElementById("js-fields-wrapper");
 
-let indexPlus;
+let indexPlus = 1;
 
 const increaseIndex = () => {
-  indexPlus += 2;
+  indexPlus++;
   //console.log(indexPlus);
   return indexPlus;
 };
 const renderInput = () => {
-  return (innerHTML = fieldTemplate);
+  return (innerHTML = `<div class="form__wrapper" id="js-form-input">
+  <div class="form__field">
+    <label class="form__label">Haslo ${indexPlus}</label>
+    <input type="text" class="form__input" id="js-input" data-index=${indexPlus} />
+  </div>
+  <button type="button" class="btn__field" id="js-delete-button">
+    <img src="../css/icon/close.svg" class="img--size" />
+  </button>
+  </div>`);
 };
 
-const fieldTemplate = `<div class="form__wrapper" id="js-form-input">
-<div class="form__field">
-  <label class="form__label">Haslo ${indexPlus}</label>
-  <input type="text" class="form__input" id="js-input" data-index=${indexPlus} />
-</div>
-<button type="button" class="btn__field" id="js-delete-button">
-  <img src="css/icon/close.svg" class="img--size" />
-</button>
-</div>`;
+const renderValidationError = () => {
+  return (innerHTML = `<span style="color: red;">Input jest pusty! Uzupełnij go prosze</span>`);
+};
+
+const getQuantityOfInputs = () => {
+  return (inputsNumber = mainForm.querySelectorAll("input").length);
+};
+
+const getValueOfInputs = () => {
+  return (inputsValue = mainForm.querySelectorAll("input").value);
+};
+
+const inputValidation = () => {
+  getValueOfInputs();
+  console.log(inputsValue);
+  if (inputsValue == undefined) {
+    fieldsWrapper.insertAdjacentHTML("beforeend", renderValidationError());
+    addButton.disabled = true;
+    return (validate = false);
+  } else {
+    return (validate = true);
+    fieldsWrapper.insertAdjacentHTML("beforeend", "<span></span>");
+  }
+};
+
+// const cursorMove = () => {
+//   inputInForm.focus();
+//   inputInForm.setSelectionRange(input, 1);
+// };
+
 const addBtnHandler = () => {
-  increaseIndex();
+  inputValidation();
+  getQuantityOfInputs();
+  if (inputsNumber >= 5) {
+    addButton.disabled = true;
+  }
   console.log(indexPlus);
-  fieldsWrapper.insertAdjacentHTML("beforeend", renderInput(indexPlus));
+  if (validate == true) {
+    increaseIndex();
+    fieldsWrapper.insertAdjacentHTML("beforeend", renderInput(indexPlus));
+  }
 };
-
+inputInForm.addEventListener("change", () => {
+  addButton.disabled = false;
+});
 addButton.addEventListener("click", addBtnHandler);
