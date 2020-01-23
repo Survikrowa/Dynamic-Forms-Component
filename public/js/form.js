bb9d81834de2e0c7d33f6c660dcee056b811c2f5
Szymon Hrabia <survikrowa@o2.pl>
2020-01-23 18:29:20 +0100

    "Final" commit

--- public/js/form.js
+++ public/js/form.js
@@ -2,60 +2,125 @@ import { renderInput } from "./renderInput";
 
 const mainForm = document.querySelector("#js-form");
 const addBtn = document.querySelector("#js-button-add");
-const formField = mainForm.querySelector("#js-fields-wrapper");
+const submitBtn = document.querySelector('[data-role="submitButton"]');
+const formWrapper = mainForm.querySelector("#js-fields-wrapper");
 
 const MAX_INPUT_COUNT = 4;
+
+const getFormField = () => {
+  return mainForm.querySelectorAll('[data-role="formField"]');
+};
 
 const getAllInputs = () => {
   return mainForm.querySelectorAll('[data-role="input"]');
 };
 
-const getInputsValue = () => {
-  return mainForm.querySelector('[data-role="input"]').value;
-};
-
-const catchNumberOfInputs = () => {
+const getNumberOfInputs = () => {
   return mainForm.querySelectorAll('[data-role="input"]').length;
 };
 
-const isInputValid = () => {
-  const inputsValue = getInputsValue();
-  const inputs = getAllInputs();
-  inputs.forEach(() => {
-    if (inputsValue === "") {
-      return false;
-    } else {
-      console.log(inputsValue);
-      return true;
-    }
-  });
-  console.log(inputs);
+const checkValueOfInputs = allInputs => {
+  return Array.from(allInputs).filter(input => input.value === "");
 };
 
-const inputNumberCheck = (inputsNumber, maxInputsNumber) => {
-  if (inputsNumber > maxInputsNumber) {
+const inputValidation = allInputs => {
+  const getEmptyInputs = checkValueOfInputs(allInputs);
+  const errorMessage = `<span style="color: white;" data-role="validation-Error">Input jest pusty, proszę o uzupełnienie!</span>`;
+  getEmptyInputs.forEach(emptyInput => {
+    emptyInput.parentNode.insertAdjacentHTML("beforeend", errorMessage);
     addBtn.disabled = true;
+  });
+};
+const areInputsCorrect = inputs => {
+  return !checkValueOfInputs(inputs).length;
+};
+const canAdd = (inputsNumber, maxInputsNumber) => {
+  if (inputsNumber > maxInputsNumber) {
+    return false;
+  } else {
+    return true;
   }
 };
 
-const addBtnHandler = inputsNumber => {
-  const inputValid = isInputValid();
-  console.log(typeof inputValid);
-  if (inputValid === true) {
-    inputNumberCheck(inputsNumber, MAX_INPUT_COUNT);
-    formField.insertAdjacentHTML("beforeend", renderInput(inputsNumber));
-  } else if (inputValid === false) {
-    formField.insertAdjacentHTML(
-      "beforeend",
-      "<span>Input jest pusty! Uzupełnij go prosze.</span>"
+const deleteErrors = () => {
+  const formFields = getFormField();
+  formFields.forEach(formField => {
+    const errorMessage = formField.querySelector(
+      '[data-role="validation-Error"]'
     );
-    addBtn.disabled = true;
+    if (errorMessage) {
+      formField.removeChild(errorMessage);
+    }
+  });
+};
+
+const addEventListenersToInputs = () => {
+  const allInputs = getAllInputs();
+  allInputs.forEach(inputs => {
+    inputs.addEventListener("input", () => {
+      addBtn.disabled = false;
+      deleteErrors();
+    });
+  });
+};
+
+const updateLabelNumber = () => {
+  const labels = mainForm.querySelectorAll(".form__label");
+  labels.forEach((label, index) => {
+    label.textContent = "Haslo " + parseInt(index + 1);
+  });
+};
+
+const deleteBtn = allInputs => {
+  const deleteButtons = mainForm.querySelectorAll('[data-role="deleteBtn"]');
+  deleteButtons.forEach(deleteButton => {
+    if (deleteButton) {
+      deleteButton.addEventListener("click", e => {
+        if (e.target.parentNode.classList.contains("form__wrapper")) {
+          e.target.parentNode.remove();
+          addBtn.disabled = false;
+          updateLabelNumber();
+        }
+      });
+    }
+  });
+};
+
+const addBtnHandler = inputsNumber => {
+  const allInputs = getAllInputs();
+  inputValidation(allInputs);
+  const isNumberOfInputsCorrect = canAdd(inputsNumber, MAX_INPUT_COUNT);
+  if (areInputsCorrect(allInputs)) {
+    formWrapper.insertAdjacentHTML("beforeend", renderInput(inputsNumber));
+    deleteErrors();
+    deleteBtn(inputsNumber);
+    if (isNumberOfInputsCorrect === true) {
+      addEventListenersToInputs();
+    }
+  }
+};
+
+const submitBtnHandler = () => {
+  const allInputs = getAllInputs();
+  inputValidation(allInputs);
+  if (areInputsCorrect(allInputs)) {
+    deleteErrors();
+    let newUrl = new URL(document.URL);
+
+    let inputsValues = [];
+    allInputs.forEach(input => inputsValues.push(input.value));
+    window.location.href = newUrl.origin + "?phrases=" + inputsValues.join(";");
   }
 };
 
 addBtn.addEventListener("click", () => {
-  const numberOfInputs = catchNumberOfInputs();
+  const numberOfInputs = getNumberOfInputs();
   addBtnHandler(numberOfInputs);
+  const currentElement = formWrapper.lastElementChild;
+  currentElement.querySelector(".form__input").focus();
 });
 
-//todo po zmianie w inpucie usunac wiadomosc
+submitBtn.addEventListener("click", e => {
+  e.preventDefault();
+  submitBtnHandler();
+});
--- public/js/renderInput.js
+++ public/js/renderInput.js
@@ -1,12 +1,13 @@
 export const renderInput = dataIndex => {
   return `<div class="form__wrapper" data-role="formWrapper">
   <div class="form__field" data-role="formField">
-    <label class="form__label">Haslo ${dataIndex + 1}</label>
-    <input type="text" class="form__input" data-role="input" data-index=${dataIndex} />
+    <label class="form__label" data-role="form-label">Haslo ${dataIndex +
+      1}</label>
+    <input type="text" class="form__input" data-role="input" data-index=${dataIndex +
+      1} />
   </div>
-  <button type="button" class="btn__field" data-role="deleteBtn-${dataIndex +
-    1}">
-    <img src="/close.a5d26944.svg"" class="img--size" />
+  <button type="button" class="btn__field" data-role="deleteBtn">
+    <img src="/close.a5d26944.svg" class="img--size" />
   </button>
   </div>`;
 };
